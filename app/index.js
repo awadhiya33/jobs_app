6507be01e447c92927e919b55f11f6e02a2a6a5a
Abhishek <abhi.harry33@gmail.com>
2023-09-03 19:03:04 +0530

    created home screen added welcome.jsx,_layout.jsx

--- app/index.js
+++ app/index.js
@@ -1,10 +1,39 @@
-import {View,Text} from 'react-native';
+import { useState } from 'react';
+import {View,ScrollView,SafeAreaView} from 'react-native';
+import { Stack,useRouter } from 'expo-router';
+import { COLORS,icons,images,SIZES } from '../constants';
+import {Nearbyjobs,Popularjobs,ScreenHeaderBtn,Welcome} from '../components'
+import { ImageBackground } from 'react-native-web';
+import { ColorSpace } from 'react-native-reanimated';
 
 const Home=()=>{
+    const router= useRouter();
+
     return(
-        <View>
-            <Text>Home</Text>
-        </View>
+        <SafeAreaView style={{flex:1,backgroundcolor:COLORS.lightWhite}}>
+            <Stack.Screen 
+            options={{
+                headerStyle:{backgroundColor:COLORS.lightWhite},
+                headerShadowVisible:false,
+                headerLeft:()=>(
+                    <ScreenHeaderBtn iconUrl={icons.menu} dimension="60%" />
+                ),
+                headerRight:()=>(
+                    <ScreenHeaderBtn iconUrl={images.profile} dimension="100%" />
+                ),
+                headerTitle:""
+            }}
+            />
+
+            <ScrollView showsVerticalScrollIndicator={false}>
+                <View style={{flex:1,padding:SIZES.medium}}>
+                    <Welcome />
+                    <Popularjobs />
+                    <Nearbyjobs />
+                </View>
+            </ScrollView>
+
+        </SafeAreaView>
     )
 }
 
